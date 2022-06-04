6547a81767783afdadce47bb7a57ac272357d2bb
kedzkiest <66341676+kedzkiest@users.noreply.github.com>
2022-06-05 08:27:48 +0900

    Create A.cpp

---
+++ AtCoder/ABC254/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    // Your code here!
+    string N;
+    cin >> N;
+    string ans;
+    ans += N[1];
+    ans += N[2];
+    cout << ans << endl;
+}
