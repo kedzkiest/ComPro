daad9a3d9efe67f76ff00e44f1223e7d3f62c440
kedzkiest <kaede12kazu22@gmail.com>
2022-09-20 10:22:21 +0900

    Create A.cpp

---
+++ AtCoder/ABC269/A.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    // Your code here!
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a+b)*(c-d) << endl << "Takahashi" << endl;
+}
